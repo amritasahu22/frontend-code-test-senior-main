cf1e700bf01d624ac563e8657985eba3c200b175
Amrita <amritasahu22@gmail.com>
2022-08-12 00:40:45 +1000

    Modifies product page  and ProductDetails components
    - Update Product Details components
    - Add quantity and addtoCart section to the product
    - Add styling

--- client/components/productDetails.jsx
+++ client/components/productDetails.jsx
@@ -1,5 +1,37 @@
 import styles from './productDetails.module.css';
 
 export default function ProductDetails({ product }) {
-	return <div>Product Details</div>;
+	return (
+		<div>
+			<div className={styles.descContainer}>
+				<h2>Description</h2>
+				<p>{product.description}</p>
+			</div>
+			<div>
+				<h2>Specification</h2>
+				<div className={styles.specContainer}>
+					<div className={styles.specRow}>Brand</div>
+					<div className={styles.specRow}>{product.brand}</div>
+				</div>
+				<div className={styles.specContainer}>
+					<div className={styles.specRow}>Item weight</div>
+					<div className={styles.specRow}>{product.weight}</div>
+				</div>
+				<div className={styles.specContainer}>
+					<div className={styles.specRow}>Dimensions</div>
+					<div
+						className={styles.specRow}
+					>{`${product.height} x ${product.width} x ${product.length}`}</div>
+				</div>
+				<div className={styles.specContainer}>
+					<div className={styles.specRow}>Item Model number</div>
+					<div className={styles.specRow}>{product.model_code}</div>
+				</div>
+				<div className={styles.specContainer}>
+					<div className={styles.specRow}>Colour</div>
+					<div className={styles.specRow}>{product.colour}</div>
+				</div>
+			</div>
+		</div>
+	);
 }
--- client/pages/product.jsx
+++ client/pages/product.jsx
@@ -16,21 +16,35 @@ export default function Product({ productData }) {
 	const { fields: product } = productData;
 
 	return (
-		<main className={styles.productContainer}>
-			<div>Product App</div>
-			<article>
-				<Image
-					src={product.img_url}
-					height={400}
-					width={400}
-					alt='Energy Saving Bulb'
-					className={styles.borderCircle}
-				/>
-				<h2>{product.name}</h2>
-				<p>
-					{product.power} // Packet of {product.quantity}
-				</p>
-				<ProductDetails> Product Details</ProductDetails>
+		<main className={styles.container}>
+			<div>Product Header</div>
+			<article className={styles.productContainer}>
+				<div>
+					<Image
+						src={product.img_url}
+						height={400}
+						width={500}
+						alt='Energy Saving Bulb'
+						className={styles.borderCircle}
+					/>
+					<h1 className={styles.productHeading}>{product.name}</h1>
+					<p className={styles.lightText}>
+						{product.power} // Packet of {product.quantity}
+					</p>
+					<div className={styles.qtyContainer}>
+						<div className={styles.price}>&pound;{product.price}</div>
+						<div>
+							<p className={styles.qtyLabel}>Qty</p>
+							<div className={styles.qtyRow}>
+								<button>+</button>
+								<span>{1}</span>
+								<button>-</button>
+							</div>
+						</div>
+					</div>
+					<button className={styles.addToCartBtn}>Add to cart</button>
+				</div>
+				<ProductDetails product={product} />
 			</article>
 		</main>
 	);
